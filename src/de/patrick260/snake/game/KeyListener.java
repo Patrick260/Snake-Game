5c38293756c715dfeb79adf2cb0fec48494f744d
Patrick260 <Patrick260@protonmail.com>
2022-01-03 10:25:09 +0100

    Fix that the snake can turn into itself

--- src/de/patrick260/snake/game/KeyListener.java
+++ src/de/patrick260/snake/game/KeyListener.java
@@ -10,19 +10,19 @@ public class KeyListener extends KeyAdapter {
 
         int key_id = event.getKeyCode();
 
-        if (key_id == KeyEvent.VK_LEFT && Game.direction != Direction.LEFT) {
+        if (key_id == KeyEvent.VK_LEFT && Game.direction != Direction.LEFT && Game.direction != Direction.RIGHT) {
 
             Game.direction = Direction.LEFT;
 
-        } else if (key_id == KeyEvent.VK_RIGHT && Game.direction != Direction.RIGHT) {
+        } else if (key_id == KeyEvent.VK_RIGHT && Game.direction != Direction.RIGHT && Game.direction != Direction.LEFT) {
 
             Game.direction = Direction.RIGHT;
 
-        } else if (key_id == KeyEvent.VK_UP && Game.direction != Direction.UP) {
+        } else if (key_id == KeyEvent.VK_UP && Game.direction != Direction.UP && Game.direction != Direction.DOWN) {
 
             Game.direction = Direction.UP;
 
-        } else if (key_id == KeyEvent.VK_DOWN && Game.direction != Direction.DOWN) {
+        } else if (key_id == KeyEvent.VK_DOWN && Game.direction != Direction.DOWN && Game.direction != Direction.UP) {
 
             Game.direction = Direction.DOWN;
 
